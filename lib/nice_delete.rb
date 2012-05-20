2f24d472684b4933d2a3bef9532741041e3c1f6b
John Isaacks <john.isaacks@programming-perils.com>
2012-05-20 16:07:50 -0400

    Make truly recursive, was not before

--- lib/nice_delete.rb
+++ lib/nice_delete.rb
@@ -1,38 +1,24 @@
 module NiceDelete
 
-  def jd_delete
-    assocs = dependent_associations
-    to_delete = assocs.reduce([]) { |b,a| b | dependent_association_ids(a) }
-    to_delete.each do |h|
-      h[:table_name].to_s.classify.constantize.delete_all(:id => h[:ids])
-    end
-    self.delete
-  end
-
-  def dependent_associations
-    # Return the names of all associations that are dependent destroy/delete
+  def nice_destroy(model_class=nil, ids=nil)
+    ids = self.id if ids.nil?
+    model_class = self.class if model_class.nil?
+    
+    # Delete self
+    model_class.delete_all(:id => ids)
+    
+    # find and delete all dependent associations
     delete_assocs = [:delete_all,:delete,:destroy,:destroy_all]
-    self.class.reflect_on_all_associations.select do |assoc| 
+    table_name = model_class.to_s.tableize
+    model_class.reflect_on_all_associations.select do |assoc|
       delete_assocs.include? assoc.options[:dependent]
-    end.map(&:name)
-  end
-
-  def dependent_association_ids assoc_name
-    # Given an association, return table name and ids for association
-    # Also do the same for sub associations 
-    dependents = self.send(assoc_name).all
-    return [] unless dependents.size > 0
-    # get table_name and ids for this association
-    table_name = assoc_name.to_s.tableize
-    ids = dependents.respond_to?(:map) ? dependents.map(&:id) : [dependents.id]
-    ret = [{:table_name => table_name, :ids => ids}]
-    # get table_name and ids for all associations of this association
-    subs = dependents.first.dependent_associations
-    subs.each do |sub|
-      sub_ids = sub.to_s.classify.constantize.send("find_all_by_#{table_name.classify.foreign_key}", ids).map(&:id)
-      ret.push({:table_name => sub, :ids => sub_ids})
+    end.map(&:name).each do |sub|
+      sub_model_class = sub.to_s.classify.constantize
+      sub_ids = sub_model_class.send("find_all_by_#{table_name.classify.foreign_key}", ids).map(&:id)
+      nice_destroy(sub_model_class, sub_ids)
     end
-    ret
+    
+    self
   end
 
 end
