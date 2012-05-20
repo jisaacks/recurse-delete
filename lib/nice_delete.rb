79f03c40ac7b67d478b23212adc9dec5f10577de
John Isaacks <john.isaacks@programming-perils.com>
2012-05-20 13:40:12 -0400

    Yay it works! but needs cleaned up

--- lib/nice_delete.rb
+++ lib/nice_delete.rb
@@ -2,7 +2,12 @@ module NiceDelete
 
   def jd_delete
     assocs = dependent_associations
-    assocs.map { |a| dependent_association_ids(a) }
+    to_delete = assocs.reduce([]) { |b,a| b | dependent_association_ids(a) }
+    to_delete.each do |h|
+      #puts "#{h}"
+      h[:table_name].to_s.classify.constantize.delete_all(:id => h[:ids])
+    end
+    self.delete
   end
 
   def dependent_associations
@@ -14,11 +19,25 @@ module NiceDelete
 
   def dependent_association_ids assoc_name
     table_name = assoc_name.to_s.pluralize
-    dependents = self.send(assoc_name)
-    subs = dependents.each { |d| d.dependent_associations }
-    puts subs.map(&:jd_delete)
+    dependents = self.send(assoc_name).all
+    return [] unless dependents.size > 0
+    subs = dependents.first.dependent_associations
+    #puts table_name.foreign_key
+    #sub_ids = subs.map { |s| dependent_association_ids(s) } 
+    #subs = dependents.each{ |d| d.dependent_associations }
+    #puts subs.map(&:jd_delete)
+    #puts subs
     ids = dependents.respond_to?(:map) ? dependents.map(&:id) : [dependents.id]
-    {:table_name => table_name, :ids => ids}
+
+    ret = [{:table_name => table_name, :ids => ids}]
+
+    subs.each do |sub|
+      sub_ids = sub.to_s.classify.constantize.send("find_all_by_#{table_name.classify.foreign_key}", ids).map(&:id)
+      ret.push({:table_name => sub, :ids => sub_ids})
+    end
+    #{:table_name => table_name, :ids => ids}
+    #dependents.first.class.find(ids)
+    ret
   end
 
   # def dependent_associations
