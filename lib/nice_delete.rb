e480c8ea3b086fbcb9fc867be6ab3d49223d3b57
John Isaacks <john.isaacks@programming-perils.com>
2012-05-20 14:00:54 -0400

    Cleaned up

--- lib/nice_delete.rb
+++ lib/nice_delete.rb
@@ -4,13 +4,13 @@ module NiceDelete
     assocs = dependent_associations
     to_delete = assocs.reduce([]) { |b,a| b | dependent_association_ids(a) }
     to_delete.each do |h|
-      #puts "#{h}"
       h[:table_name].to_s.classify.constantize.delete_all(:id => h[:ids])
     end
     self.delete
   end
 
   def dependent_associations
+    # Return the names of all associations that are dependent destroy/delete
     delete_assocs = [:delete_all,:delete,:destroy,:destroy_all]
     self.class.reflect_on_all_associations.select do |assoc| 
       delete_assocs.include? assoc.options[:dependent]
@@ -18,48 +18,21 @@ module NiceDelete
   end
 
   def dependent_association_ids assoc_name
-    table_name = assoc_name.to_s.pluralize
+    # Given an association, return table name and ids for association
+    # Also do the same for sub associations 
     dependents = self.send(assoc_name).all
     return [] unless dependents.size > 0
-    subs = dependents.first.dependent_associations
-    #puts table_name.foreign_key
-    #sub_ids = subs.map { |s| dependent_association_ids(s) } 
-    #subs = dependents.each{ |d| d.dependent_associations }
-    #puts subs.map(&:jd_delete)
-    #puts subs
+    # get table_name and ids for this association
+    table_name = assoc_name.to_s.tableize
     ids = dependents.respond_to?(:map) ? dependents.map(&:id) : [dependents.id]
-
     ret = [{:table_name => table_name, :ids => ids}]
-
+    # get table_name and ids for all associations of this association
+    subs = dependents.first.dependent_associations
     subs.each do |sub|
       sub_ids = sub.to_s.classify.constantize.send("find_all_by_#{table_name.classify.foreign_key}", ids).map(&:id)
       ret.push({:table_name => sub, :ids => sub_ids})
     end
-    #{:table_name => table_name, :ids => ids}
-    #dependents.first.class.find(ids)
     ret
-  end
-
-  # def dependent_associations
-  #   delete_assocs = [:delete_all,:delete,:destroy,:destroy_all]
-  #   # Self.reflect_on_all_associations.select do |assoc| 
-  #   #   delete_assocs.include? assoc.options[:dependent]
-  #   # end.map(&:name)
-
-  #   self.class.reflect_on_all_associations.select do |assoc| 
-  #     delete_assocs.include? assoc.options[:dependent]
-  #   end.map do |assoc|
-  #     table_name = assoc.name.to_s.pluralize
-  #     dependents = self.send(assoc.name)
-  #     subs = dependents.each { |d| d.dependent_associations }
-  #     puts subs
-  #     ids = dependents.respond_to?(:map) ? dependents.map(&:id) : [dependents.id]
-  #     {:table_name => table_name, :ids => ids}
-  #   end
-  # end
-
-  def subdependent_associations parent_name, child_name, ids
-
   end
 
 end
